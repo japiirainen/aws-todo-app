a356feebffc5be2dd8b8902006d5784828b0465b
Joona Piirainen <joona.piirainen@gmail.com>
2020-09-25 14:32:00 +0300

    basic crud with lambda and dynamodb

--- lib/aws-todo-stack.ts
+++ lib/aws-todo-stack.ts
@@ -1,24 +1,51 @@
 import * as cdk from '@aws-cdk/core'
-import * as lambda from '@aws-cdk/aws-lambda'
 import * as apiGateway from '@aws-cdk/aws-apigateway'
+import * as s3 from '@aws-cdk/aws-s3'
+import * as s3Deployment from '@aws-cdk/aws-s3-deployment'
+import {TodoBackend} from './todo-backend'
 
 export class AwsTodoStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props)
 
-    const helloLambda = new lambda.Function(this, 'HelloLamda', {
-      code: lambda.Code.fromAsset('lambda'),
-      handler: 'hello.handler',
-      runtime: lambda.Runtime.NODEJS_12_X,
-      memorySize: 256,
-      timeout: cdk.Duration.seconds(10),
-      environment: {
-        isProduction: 'no',
-      },
-    })
+    const todoBackend = new TodoBackend(this, 'TodoBackend')
 
     new apiGateway.LambdaRestApi(this, 'Endpoint', {
-      handler: helloLambda,
+      handler: todoBackend.handler,
     })
+
+    const logoBucket = new s3.Bucket(this, 'LogoBucket', {
+      publicReadAccess: true,
+    })
+
+    new s3Deployment.BucketDeployment(this, 'SomeDeployment', {
+      destinationBucket: logoBucket,
+      sources: [s3Deployment.Source.asset('./assets')],
+    })
+
+    new cdk.CfnOutput(this, 'LogoPath', {
+      value: `https://${logoBucket.bucketDomainName}`,
+    })
+
+    //this is how could host static html
+    const websiteBucket = new s3.Bucket(this, 'WebsiteBucket', {
+      publicReadAccess: true,
+      websiteIndexDocument: 'index.html',
+    })
+
+    new s3Deployment.BucketDeployment(this, 'WebsiteDeployment', {
+      destinationBucket: websiteBucket,
+      sources: [s3Deployment.Source.asset('./frontend')],
+    })
+
+    new cdk.CfnOutput(this, 'WebsiteAddress', {
+      value: websiteBucket.bucketWebsiteUrl,
+    })
+
+    //or this is better for a spa
+    // new SPADeploy(this, 'WebsiteDeploy').createSiteWithCloudfront({
+    //   indexDoc: 'index.html',
+    //   websiteFolder: './frontend',
+    // })
   }
 }
